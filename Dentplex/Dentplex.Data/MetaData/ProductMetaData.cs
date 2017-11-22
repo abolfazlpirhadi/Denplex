eaff6ca926a84f605ccc6fe088d68344df268ac6
abolfazlpirhadi <en.ab.pirhady@gmail.com>
2017-11-22 23:45:16 +0330

    Start Project-Pirhadi

---
+++ Dentplex/Dentplex.Data/MetaData/ProductMetaData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dentplex.Data.Model
+{
+    public class ProductMetaData
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public int ProductGroupID { get; set; }
+        public int ProductSubGroupID { get; set; }
+
+        [Display(Name = "نام محصول")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید.")]
+        public string ProductTitle { get; set; }
+        public string ProductShortText { get; set; }
+        public string ProductText { get; set; }
+        public string ProductImage { get; set; }
+    }
+
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+    }
+}
